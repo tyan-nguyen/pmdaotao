c66c9da69eff14a9e127bd110dc8a12b10b73584
Khanh Quy <khanhankhanhquy123@gmail.com>
2024-08-19 15:15:07 +0700

    update migrate

--- modules/vanban/models/base/VanBanDenBase.php
+++ modules/vanban/models/base/VanBanDenBase.php
@@ -38,19 +38,19 @@ class VanBanDenBase extends VanBanBase
      * {@inheritdoc}
      */
   
-    public function rules()
-    {
-        return [
-            [['so_vb','trich_yeu','nguoi_ky','ngay_ky','vbden_so_den'], 'required'],
-            [['id_loai_van_ban', 'vbden_so_den', 'vbden_nguoi_nhan','nguoi_tao'], 'integer'],
-            [['ngay_ky', 'vbden_ngay_den', 'vbden_ngay_chuyen', 'thoi_gian_tao'], 'safe'],
-            [['so_vb', 'trich_yeu', 'nguoi_ky', 'ghi_chu','so_loai_van_ban', 'string', 'max' => 255],
-            [['id_loai_van_ban'], 'exist', 'skipOnError' => true, 'targetClass' => DmLoaiVanBan::class, 'targetAttribute' => ['id_loai_van_ban' => 'id']],
-            [['vbden_nguoi_nhan'], 'exist', 'skipOnError' => true, 'targetClass' => NhanVien::class, 'targetAttribute' => ['vbden_nguoi_nhan' => 'id']],
-            [['vbden_so_den'], 'validateUniqueVbdenSoDen'],
-        
-        ];
-    }
+     public function rules()
+     {
+         return [
+             [['so_vb', 'trich_yeu', 'nguoi_ky', 'ngay_ky', 'vbden_so_den'], 'required'],
+             [['id_loai_van_ban', 'vbden_so_den', 'vbden_nguoi_nhan', 'nguoi_tao'], 'integer'],
+             [['ngay_ky', 'vbden_ngay_den', 'vbden_ngay_chuyen', 'thoi_gian_tao'], 'safe'],
+             [['so_vb', 'trich_yeu', 'nguoi_ky', 'ghi_chu', 'so_loai_van_ban'], 'string', 'max' => 255],
+             [['id_loai_van_ban'], 'exist', 'skipOnError' => true, 'targetClass' => DmLoaiVanBan::class, 'targetAttribute' => ['id_loai_van_ban' => 'id']],
+             [['vbden_nguoi_nhan'], 'exist', 'skipOnError' => true, 'targetClass' => NhanVien::class, 'targetAttribute' => ['vbden_nguoi_nhan' => 'id']],
+             [['vbden_so_den'], 'validateUniqueVbdenSoDen'],
+         ];
+     }
+     
     public function validateUniqueVbdenSoDen($attribute)
     {
         $query = self::find()->where([$attribute => $this->$attribute]);
