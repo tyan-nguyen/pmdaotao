4ab425809d81272a08058968dd4e49ff79995f5b
tyan-nguyen <nguyenvantyan@gmail.com>
2024-08-19 15:00:02 +0700

    update git inorge

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=pmdaotao',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=pmdaotao1111',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
