bb6eb364089883d434cb04d4149234741c2c8109
tyan-nguyen <nguyenvantyan@gmail.com>
2025-07-30 08:00:03 +0700

    cap nhat menu thue

--- themes/main/layouts/menus/_menu-thue.php
+++ themes/main/layouts/menus/_menu-thue.php
@@ -95,3 +95,46 @@ use app\modules\hocvien\models\HocVien;
 
 	</ul>
 </li>
+
+<li class="slide">
+	<a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0);">
+		<!-- <span class="side-menu__icon"><i class="fe fe-users side_menu_img"></i></span>
+		<span class="side-menu__label">Quản lý giáo viên</span><i class="angle fe fe-chevron-right"></i> -->
+		<span class="side-menu__label"><i class="fa fa-folder"></i> Thông tin xe</span><i class="angle fa fa-caret-right"></i>
+	</a>
+	<ul class="slide-menu" data-menu="xe">
+		<li class="panel sidetab-menu">
+			<div class="tab-menu-heading p-0 pb-2 border-0">
+				<div class="tabs-menu ">
+					<ul class="nav panel-tabs">
+						<li><a href="#side5" class="active" data-bs-toggle="tab"><i
+									class="bi bi-house"></i>
+								<p>Home</p>
+							</a></li>
+						<li><a href="#side6" data-bs-toggle="tab"><i class="bi bi-box"></i>
+								<p>Activity</p>
+							</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="panel-body tabs-menu-body p-0 border-0">
+				<div class="tab-content">
+					<div class="tab-pane active" id="side5">
+						<ul class="sidemenu-list">
+							<li class="side-menu__label1"><a href="javascript:void(0)">Danh mục chức năng</a>
+							</li>
+							<li><a href="<?= Yii::getAlias('@web/thuexe/xe?menu=xe1') ?>" class="slide-item" data-menu="xe1"><i class="fe fe-file-text"></i> Thông tin xe</a></li>
+						</ul>
+						<div class="menutabs-content px-0">
+							<!-- menu tab here -->
+						</div>
+					</div>
+					<div class="tab-pane" id="side6">
+						<!-- activity here -->
+					</div>
+				</div>
+			</div>
+		</li>
+
+	</ul>
+</li>
