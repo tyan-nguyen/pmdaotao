4ffefa767cebb513df1d598bd33a7a6488361217
Nvtan <nguyenvantyan@gmail.com>
2026-01-21 13:26:58 +0700

    cap nhat khoa hoc

--- modules/khoahoc/models/search/KhoaHocSearch.php
+++ modules/khoahoc/models/search/KhoaHocSearch.php
@@ -45,6 +45,7 @@ class KhoaHocSearch extends KhoaHoc
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort'=> ['defaultOrder' => ['id' => SORT_DESC]],
         ]);
 
         $this->load($params);
