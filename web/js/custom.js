733f9df24959750fe301cb3b60a92dfec7354cfa
Nvtan <nguyenvantyan@gmail.com>
2026-02-14 08:50:46 +0700

    cap nhat tooltip view from ke hoach

--- web/js/custom.js
+++ web/js/custom.js
@@ -118,3 +118,11 @@ setMenuActive();
 function toggleSearch(){
 	$("#divFilterExtend").toggle();
 }
+
+//tooltip
+document.addEventListener('DOMContentLoaded', function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    tooltipTriggerList.forEach(function (el) {
+        new bootstrap.Tooltip(el)
+    })
+})
