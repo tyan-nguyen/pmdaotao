3ab9d6c945672d0c5f650347f91057a9427be4b9
Nvtan <nguyenvantyan@gmail.com>
2026-02-14 08:54:55 +0700

    cap nhat tooltip view from ke hoach

--- modules/daotao/views/tiet-hoc/_viewFromKeHoach.php
+++ modules/daotao/views/tiet-hoc/_viewFromKeHoach.php
@@ -148,4 +148,14 @@ document.addEventListener('DOMContentLoaded', function () {
         new bootstrap.Tooltip(el)
     })
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+
+    tooltipTriggerList.forEach(function (el) {
+        new bootstrap.Tooltip(el, {
+            customClass: 'tooltip-primary'
+        })
+    })
+})
 </script>
