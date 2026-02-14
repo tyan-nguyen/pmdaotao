d0596e1296c50d3b3c96bf949918d78d2f098cd1
Nvtan <nguyenvantyan@gmail.com>
2026-02-14 08:29:03 +0700

    cap nhat tooltip view from ke hoach

--- assets/ViboonAsset.php
+++ assets/ViboonAsset.php
@@ -59,7 +59,7 @@ class ViboonAsset extends AssetBundle
         'https://cdn.jsdelivr.net/npm/flatpickr',
         //fancybox
         'js/fancybox/fancybox.umd.js',
-        'js/custom.js?v=2.2',
+        'js/custom.js?v=2.3',
         //'assets/plugins/tabs/jquery.multipurpose_tabcontent.js',
         //'assets/plugins/tabs/tab-content.js'
     ];
--- web/js/custom.js
+++ web/js/custom.js
@@ -118,3 +118,10 @@ setMenuActive();
 function toggleSearch(){
 	$("#divFilterExtend").toggle();
 }
+
+//tooltip
+// ______________Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	return new bootstrap.Tooltip(tooltipTriggerEl)
+})
