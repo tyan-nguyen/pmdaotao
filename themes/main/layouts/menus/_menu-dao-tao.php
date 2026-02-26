b77982da2a35b6c951a24e278569021e9dff70f7
Nvtan <nguyenvantyan@gmail.com>
2026-02-26 14:20:23 +0700

    capnhat phan quyen lich dung xe

--- themes/main/layouts/menus/_menu-dao-tao.php
+++ themes/main/layouts/menus/_menu-dao-tao.php
@@ -44,3 +44,44 @@ use app\modules\hocvien\models\HocVien;
 
 	</ul>
 </li>
+
+<li class="slide">
+	<a class="side-menu__item" data-bs-toggle="slide" href="javascript:void(0);">
+		
+		<span class="side-menu__label"><i class="fa fa-folder"></i> Đăng ký sử dụng xe</span><i class="angle fa fa-caret-right"></i>
+	</a>
+	<ul class="slide-menu" data-menu="dksdx">
+		<li class="panel sidetab-menu">
+			<div class="tab-menu-heading p-0 pb-2 border-0">
+				<div class="tabs-menu ">
+					<ul class="nav panel-tabs">
+						<li><a href="#side3" class="active" data-bs-toggle="tab"><i class="bi bi-house"></i>
+								<p>Home</p>
+							</a></li>
+						<li><a href="#side4" data-bs-toggle="tab"><i class="bi bi-box"></i>
+								<p>Activity</p>
+							</a></li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="panel-body tabs-menu-body p-0 border-0">
+				<div class="tab-content">
+					<div class="tab-pane active" id="side3">
+						<ul class="sidemenu-list">
+							<li><a href="<?= Yii::getAlias('@web/thuexe/lich-dung-xe/index?menu=dksdx1') ?>" class="slide-item" data-menu="dksdx1"> <i class="fe fe-file-text"></i> Đăng ký sử dụng xe <span class="badge bg-warning">New</span></a></li>
+						
+						</ul>
+						<div class="menutabs-content px-0">
+							<!-- menu tab here -->
+						</div>
+					</div>
+					<div class="tab-pane" id="side4">
+						<!-- activity here -->
+					</div>
+				</div>
+			</div>
+		</li>
+
+	</ul>
+</li>
