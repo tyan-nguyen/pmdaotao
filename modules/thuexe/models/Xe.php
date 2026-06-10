86b75d8281373f0c511aac970c3252e30d4ab2d3
tyan-nguyen <nguyenvantyan@gmail.com>
2026-06-10 16:54:13 +0700

    cap nhat bao cao vi pham

--- modules/thuexe/models/Xe.php
+++ modules/thuexe/models/Xe.php
@@ -629,7 +629,7 @@ class Xe extends \app\models\PtxXe
         //lịch sửa chữa, bảo dưỡng add to list lịch (sét màu đỏ)
         $lichSuaXes = PhieuDeNghi::find()->where([
             'loai_tai_san' => PhieuDeNghi::LOAITAISAN_XE,
-            'loai_phieu' => PhieuDeNghi::LOAIPHIEU_SUACHUA,
+            // 'loai_phieu' => PhieuDeNghi::LOAIPHIEU_SUACHUA,
             'id_tham_chieu' => $this->id,
         ])->andWhere(['IN', 'trang_thai', PhieuDeNghi::getDmTrangThaiCoSoVaoSo()])
             ->andWhere(['>=', 'ngay_bat_dau', $start])
